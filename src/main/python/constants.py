be4ac62ece7be54522abdcc0ebe330b9803c8b4c
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2021-02-26 11:39:01 +0100

    remove obsolete constant declaration

--- src/main/python/constants.py
+++ src/main/python/constants.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from config_parse import CFG
 GLOBAL_FONT = {'family':CFG("font"),'weight':'normal','size': CFG("global_text_size")}
-BASE_PATH=CFG("default_source_path")
 SOURCE_PATH=CFG("default_target_dir")
 FIGURE=0
 AXIS=1
