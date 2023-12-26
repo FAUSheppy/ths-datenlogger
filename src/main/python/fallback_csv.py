6aff13a8e1a6a0a75498ed9b69c290ae026336cf
Yannik Schmidt <sheppy@atlantishq.de>
2023-12-26 15:32:38 +0100

    wip:

---
+++ src/main/python/fallback_csv.py
@@ -0,0 +1,25 @@
+import glob
+import os
+
+
+def read_file=
+
+def generate(master_dir, from_time, to_time, cache_dir_fullpath):
+
+    timeframes = dict()
+
+    # read files
+    files = glob.glob(master_dir + "/*.txt"):
+    print(files)
+
+    for f in files:
+        timeframes
+
+
+    # glob files
+    # parse start end dates
+    # find a file that is a superset
+    # load the file
+    # generate a correct cache file from it return
+    # fix the dates
+    # if no suiteable file is found return an appropriate error
